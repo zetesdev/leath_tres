f109e0c7fa5f978dfb7a5f419c2d7afeb0cf2e84
zetes_lenovo <zbigniew.sulkowski@gmail.com>
2023-09-20 11:59:43 +0200

    initial
    initial commit

---
+++ src/components/BaseScene.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core';
+import { OrbitControls } from '@tresjs/cientos';
+</script>
+
+<template>
+  <div class="w-[1024px] h-[512px]">
+    <TresCanvas clear-color="#82DBC5">
+      <TresPerspectiveCamera :position="[3, 3, 3]" :look-at="[0, 0, 0]" />
+      <OrbitControls />
+
+      <TresMesh>
+        <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+        <TresMeshBasicMaterial color="orange" />
+      </TresMesh>
+      <TresAmbientLight :intensity="1" />
+    </TresCanvas>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { templateCompilerOptions } from '@tresjs/core';
+
+// https://vitejs.dev/config/
+
+// export default defineConfig({
+//   plugins: [vue()],
+// });
+
+export default defineConfig({
+  plugins: [
+    vue({
+      // Other config
+      ...templateCompilerOptions,
+    }),
+  ],
+});
