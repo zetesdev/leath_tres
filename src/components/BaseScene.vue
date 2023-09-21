7a5a1d4f6d6019d8d5609abf441b74962774e7b7
zetes_lenovo <zbigniew.sulkowski@gmail.com>
2023-09-21 10:30:39 +0200

    Loading custom model.

--- src/components/BaseScene.vue
+++ src/components/BaseScene.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
+import MultiTool from './MultiTool.vue';
+
 import { TresCanvas } from '@tresjs/core';
 import { OrbitControls } from '@tresjs/cientos';
 </script>
 
 <template>
   <div class="w-[1024px] h-[512px]">
-    <TresCanvas clear-color="#82DBC5">
+    <TresCanvas clear-color="#334455">
       <TresPerspectiveCamera :position="[3, 3, 3]" :look-at="[0, 0, 0]" />
       <OrbitControls />
-
+      <Suspense>
+        <MultiTool></MultiTool>
+      </Suspense>
       <TresMesh>
         <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
         <TresMeshBasicMaterial color="orange" />
---
+++ src/components/MultiTool.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useGLTF } from '@tresjs/cientos';
+
+const { scene } = await useGLTF('src/assets/tool_01.glb');
+console.log(scene);
+</script>
+
+<template>
+  <primitive :object="scene" />
+</template>
