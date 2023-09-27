c0e708fb47397913361d4b28903406d03b4d77e6
zetes_lenovo <zbigniew.sulkowski@gmail.com>
2023-09-27 12:41:31 +0200

    Initial working logic for controlling buttons.

--- src/components/PlierController.vue
+++ src/components/PlierController.vue
@@ -1,12 +1,70 @@
 <script setup lang="ts">
-import { toRefs } from 'vue';
+import { computed, toRefs } from 'vue';
 import { useChoicesStore } from '../stores/choices.ts';
 
-const { toggleOpen, open } = toRefs(useChoicesStore());
+const {
+  open,
+  scissors,
+  screwdriver,
+  toggleOpen,
+  toggleScissors,
+  toggleScrewdriver,
+} = toRefs(useChoicesStore());
+
+const pliersClass = computed(() => {
+  return {
+    active: !scissors.value || !screwdriver.value,
+    inactive: scissors.value || screwdriver.value,
+  };
+});
+
+const scissorsClass = computed(() => {
+  return {
+    active: !open.value,
+    inactive: open.value,
+  };
+});
+
+const screwdriverClass = computed(() => {
+  return {
+    active: !open.value,
+    inactive: open.value,
+  };
+});
 </script>
 
 <template>
-  <button class="bg-red-500 text-3xl" @click="toggleOpen">
-    {{ open ? 'CLOSE' : 'OPEN' }}
-  </button>
+  <div class="flex">
+    <button
+      :class="pliersClass"
+      @click="toggleOpen"
+      :disabled="scissors || screwdriver"
+    >
+      {{ open }}
+    </button>
+    <button :class="scissorsClass" @click="toggleScissors" :disabled="open">
+      {{ scissors }}
+    </button>
+    <button
+      :class="screwdriverClass"
+      @click="toggleScrewdriver"
+      :disabled="open"
+    >
+      {{ screwdriver }}
+    </button>
+  </div>
 </template>
+
+<style scoped>
+button {
+  @apply text-3xl p-5;
+}
+
+.active {
+  @apply bg-red-500;
+}
+
+.inactive {
+  @apply bg-red-200;
+}
+</style>
--- src/stores/choices.ts
+++ src/stores/choices.ts
@@ -3,10 +3,27 @@ import { defineStore } from 'pinia';
 
 export const useChoicesStore = defineStore('choices', () => {
   const open = ref(false);
+  const scissors = ref(false);
+  const screwdriver = ref(false);
 
   const toggleOpen = () => {
     open.value = !open.value;
   };
 
-  return { open, toggleOpen };
+  const toggleScissors = () => {
+    scissors.value = !scissors.value;
+  };
+
+  const toggleScrewdriver = () => {
+    screwdriver.value = !screwdriver.value;
+  };
+
+  return {
+    open,
+    scissors,
+    screwdriver,
+    toggleOpen,
+    toggleScissors,
+    toggleScrewdriver,
+  };
 });
